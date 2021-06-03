eeace9656e4f2254712559d612a7e9cf6166f8f3
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2021-06-03 20:29:03 +0200

    added nullable fields

--- backend/task/models.py
+++ backend/task/models.py
@@ -5,15 +5,15 @@ class Task(models.Model):
   requestor_id = models.BigIntegerField()
   description = models.TextField()
   date = models.DateField(auto_now_add=True)  # should be set to date at creation of object automatically
-  acceptor_id = models.BigIntegerField(blank=True)
-  title = models.CharField(max_length=50, blank=True)
-  phone_number = models.CharField(max_length=16, blank=True)
-  store_addr = models.CharField(max_length=100, blank=True)
-  delivery_addr = models.CharField(max_length=100, blank=True)
-  s_latitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True)
-  s_longitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True)
-  d_latitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True)
-  d_longitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True)
+  acceptor_id = models.BigIntegerField(blank=True, null=True)
+  title = models.CharField(max_length=50, blank=True, null=True)
+  phone_number = models.CharField(max_length=16, blank=True, null=True)
+  store_addr = models.CharField(max_length=100, blank=True, null=True)
+  delivery_addr = models.CharField(max_length=100, blank=True, null=True)
+  s_latitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True, null=True)
+  s_longitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True, null=True)
+  d_latitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True, null=True)
+  d_longitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True, null=True)
   
   #task types
   PHONE_CALL = 'PC'
