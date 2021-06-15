200b8d9c1e0aea00adb1561599ea331777f9d080
NaraS91 <39410439+NaraS91@users.noreply.github.com>
2021-06-15 15:22:04 +0200

    fixed environ

--- backend/website/settings/dev.py
+++ backend/website/settings/dev.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
-from environ import Env
+import environ
 
 env = environ.Env(
     # set casting, default value
